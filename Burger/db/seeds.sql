6a3b4c0e45b6b4584f7861ceb0c296592e74adde
Geiji <2is4you@gmail.com>
2020-05-09 12:08:09 -0700

    putting together files in order

---
+++ Burger/db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ("Levi Bird");
+INSERT INTO burgers (burger_name) VALUES ("Baked Alaska");
+INSERT INTO burgers (burger_name) VALUES ("Tweedles");
+INSERT INTO burgers (burger_name) VALUES ("Sassafras");
+INSERT INTO burgers (burger_name) VALUES ("Shiba");
